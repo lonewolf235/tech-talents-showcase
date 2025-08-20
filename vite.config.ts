274402578a323f4ca669840b9926e73ee510a55e
lonewolf235 <gagantomar235@gmail.com>
2025-08-21 01:54:52 +0530

    Removed server code

--- vite.config.ts
+++ vite.config.ts
@@ -1,20 +1,22 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
 import path from "path";
+import { fileURLToPath } from 'url';
+import { dirname } from 'path';
 
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
   resolve: {
     alias: {
-      "@": path.resolve(__dirname, "./client/src"),
-      "@shared": path.resolve(__dirname, "./shared"),
-      "@assets": path.resolve(__dirname, "./attached_assets"),
+      "@": path.resolve(dirname(fileURLToPath(import.meta.url)), "./client/src"),
+      "@shared": path.resolve(dirname(fileURLToPath(import.meta.url)), "./shared"),
+      "@assets": path.resolve(dirname(fileURLToPath(import.meta.url)), "./attached_assets"),
     },
   },
-  root: path.resolve(__dirname, "./client"),
+  root: path.resolve(dirname(fileURLToPath(import.meta.url)), "./client"),
   build: {
-    outDir: path.resolve(__dirname, "./dist"),
+    outDir: path.resolve(dirname(fileURLToPath(import.meta.url)), "./dist"),
     emptyOutDir: true,
   },
 });
